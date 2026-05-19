f6051ec59c1929d0ae35ef22df5ff933358df608
Jose Buzzi <rytmrandomizer@local.dev>
2026-05-19 16:29:08 -0400

    feat: add arm-gated hardware sender

--- rytm_randomizer/senders/__init__.py
+++ rytm_randomizer/senders/__init__.py
@@ -3,5 +3,12 @@
 from __future__ import annotations
 
 from .guarded import GuardedSendResult, guarded_send
+from .hardware import HardwareSendResult, TripleSender, hardware_send
 
-__all__ = ["GuardedSendResult", "guarded_send"]
+__all__ = [
+    "GuardedSendResult",
+    "HardwareSendResult",
+    "TripleSender",
+    "guarded_send",
+    "hardware_send",
+]
